b88e11ef2d847c6a84bebbab9bd54e1e2b6cdf49
Ben Weekes <benweekes73@gmail.com>
2022-03-23 12:33:39 +0000

    run direct on 443

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-var path = require("path");
-var express = require("express");
+const http = require("http");                 // http server core module
+const https = require('https');
+const fs = require('fs');
+const path = require("path");
+const express = require("express");           // web framework external module
+const portHTTPS =  443
 
+const app = express();
 var DIST_DIR = path.join(__dirname, "dist");
-var PORT = 8080;
-var app = express();
-
-//Serving the files on the dist folder
 app.use(express.static(DIST_DIR));
 
-//Send index.html when the user access the web
-//app.get("*", function (req, res) {
-//    res.sendFile(path.join(DIST_DIR, "index.html"));
-//});
+const httpsServer = require("https").createServer({
+     key: fs.readFileSync("/home/ubuntu/key.pem"),
+     cert: fs.readFileSync("/home/ubuntu/cert.pem")
+},app);
 
-app.listen(PORT);
+ httpsServer.listen(portHTTPS, () => {
+     console.log("listening on https://localhost:" + portHTTPS);
+    });
